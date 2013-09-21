ef92b1717355b14163b16b27351569ef521e0e18
Daniel Bordak <dbordak@gmail.com>
2013-09-21 15:34:57 -0400

    Added timestamp

--- taskerflags.py
+++ taskerflags.py
@@ -11,12 +11,13 @@ example_phone = {
 	"name" : "phon",
 	"battery" : "1",
 	"busy" : "0",
+	"timestamp" : "i do not know what the format of this is supposed to beeeeeee",
 	"message" : "hi i am not home right now please leave a message after the beep. beep."
 }
 UID = devices.insert(example_phone) #Insertion returns a unique ID; this may not be needed. Only time will tell.
 #TODO do things with this
 
-def fetch(name)
+def fetch(name):
 	
 
 @app.route('/')
