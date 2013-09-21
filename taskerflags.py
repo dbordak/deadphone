dfdd99c73ab546a944c31b1544f207d4c8c5b672
Revan Sopher <rsopher@gmail.com>
2013-09-21 12:52:34 -0400

    Add /username skeleton

--- taskerflags.py
+++ taskerflags.py
@@ -6,3 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+@app.route('/<username>', methods=['GET', 'POST'])
+def profile(username):
+	if request.method == 'POST':
+		return 'TODO: add message to database'
+	else:
+		return username #TODO fetch messages
