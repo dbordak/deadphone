a50a7e6aa5965fa097682eff0981c25cbcdd00df
Daniel Bordak <dbordak@gmail.com>
2013-09-22 01:58:44 -0400

    Refactored shit and added handling for bat, avail, and name

--- taskerflags.py
+++ taskerflags.py
@@ -13,7 +13,7 @@ devices = db.devices
 batstring_good = " they're charged up and good to go."
 batstring_bad  = " they have found themselves plagued by their current lack of current."
 
-string_busy      = " is currently busy, "
+string_busy      = " is currently avail, "
 string_available = " is able to talk, as far as we know, "
 
 conjunction_p = "and"
@@ -24,7 +24,7 @@ conjunction_n = "but"
 # msg  - short informational message
 # time - timestamp of last update
 # bat  - current battery status. -1 for critical, 1 for feelin' fine.
-# busy - current google calendar busy status. -1 for busy, 1 for available.
+# avail - current google calendar availability status. -1 for avail, 1 for available.
 
 def update_device(ID, msg, **kwargs):
 	device = {
@@ -34,8 +34,8 @@ def update_device(ID, msg, **kwargs):
 	if len(kwargs):
 		if kwargs.keys()[0] == 'bat':
 			device['bat']=kwargs['bat']
-		if kwargs.keys()[0] == 'busy':
-			device['busy']=kwargs['busy']
+		if kwargs.keys()[0] == 'avail':
+			device['avail']=kwargs['avail']
 		if kwargs.keys()[0] == 'name':
 			device['name']=kwargs['name']
 	else:
@@ -51,8 +51,8 @@ def brace_handler(ID, body):
 		opt = body.strip('{}').split(',')
 		if opt[0] == "bat":
 			update_device(ID, body, bat=opt[1])
-		if opt[0] == "busy":
-			update_device(ID, body, busy=opt[1])
+		if opt[0] == "avail":
+			update_device(ID, body, avail=opt[1])
 		if opt[0] == "name":
 			update_device(ID, body, name=opt[1])
 	else:
@@ -79,4 +79,31 @@ def profile(ID):
 			return 'success'
 	else:
 		device = devices.find_one({'ID' : ID})
-		return render_template('index.html', ID=ID, msg=device['msg'], time=device['time']) #, name=device['name'], bat=device['bat'], busy=device['busy'])
+		keys = device.keys()
+		if "name" in keys:
+			name = device['name']
+		else:
+			name = ID
+		if "bat" in keys:
+			if device['bat'] == "1":
+				bat = batstring_good
+			if device['bat'] == "-1":
+				bat = batstring_bad
+		else:
+			bat = ""
+			conj = ""
+		if "avail" in keys:
+			if device['avail'] == "1":
+				avail = string_available
+			if device['avail'] == "-1":
+				avail = string_busy
+			if "bat" in keys:
+				conj_int = int(avail)*int(device['bat'])
+				if conj_int == 1:
+					conj = conjunction_p
+				if conj_int == -1:
+					conj = conjunction_n
+		else:
+			avail = ""
+			conj = ""
+		return render_template('index.html', name=name, msg=device['msg'], time=device['time'], bat=bat, avail=avail, conj=conj)
