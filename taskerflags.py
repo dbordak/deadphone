f33605543d53cfa39dec28a1b8cb408516b10a3d
Daniel Bordak <dbordak@gmail.com>
2013-09-21 22:08:06 -0400

    Refactored name to ID

--- taskerflags.py
+++ taskerflags.py
@@ -10,18 +10,24 @@ client = MongoClient(os.environ['MONGOHQ_URL'])
 db = client.get_default_database()
 devices = db.devices
 
-def update_device(name, msg):
+## Device object
+#
+#
+#
+
+def update_device(ID, msg, **kwargs):
+	if 
 	return devices.find_and_modify(
-		query={'name': name},
+		query={'ID': ID},
 		update={
-			'name' : name,
+			'ID' : ID,
 			'msg' : msg,
 			'time' : datetime.strftime(datetime.now(),'%a %b %d, %Y - %I:%M %p')
 		},
 		upsert=True
 	)
 
-def brace_handler(name, body):
+def brace_handler(ID, body):
 	if body.startswith('{'):
 		op = body.strip('{}').split(',')
 		if op[0] == "bat":
@@ -29,27 +35,27 @@ def brace_handler(name, body):
 		if op[0] == "busy":
 			#TODO: update_device()
 	else:
-		update_device(name, body)
+		update_device(ID, body)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if request.method == 'GET':
 		return render_template('index.html')
 	else:
-		name = request.values.get('From', None)
+		ID = request.values.get('From', None)
 		body = request.values.get('Body', None)
-		sms_handler(name, body)
+		sms_handler(ID, body)
 
-@app.route('/<name>', methods=['GET', 'POST'])
-def profile(name):
+@app.route('/<ID>', methods=['GET', 'POST'])
+def profile(ID):
 	if request.method == 'POST':
 		if len(request.form['msg'])>160:
 			return 'fail: message > 160'
-		else if name.startswith('+'):
+		else if ID.startswith('+'):
 			return 'fail: + reserved for SMS'
 		else:
-			brace_handler(name, request.form['msg'])
+			brace_handler(ID, request.form['msg'])
 			return 'success'
 	else:
-		device = devices.find_one({'name' : name})
-		return render_template('index.html', name=name, msg=device['msg'], time=device['time'])
+		device = devices.find_one({'ID' : ID})
+		return render_template('index.html', ID=ID, msg=device['msg'], time=device['time'])
